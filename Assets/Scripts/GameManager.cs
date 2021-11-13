019743557e18ad56700f5d64195c7112e598351c
Alexander Usovich <usovich@vandrouka.games>
2021-11-13 17:50:31 +0400

    Added GameManager singletone and loader

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+
+    public BoardManager boardManager;
+
+    private int level = 3;
+
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        boardManager = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    private void InitGame()
+    {
+        boardManager.SetupScene(level);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    void Awake()
+    {
+        if (GameManager.instance == null)
+        {
+            Instantiate(gameManager);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}

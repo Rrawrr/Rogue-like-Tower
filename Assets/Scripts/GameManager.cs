16b35d9db9eb50bfccb57fb4bc017c7ba0309db9
Alexander Usovich <usovich@vandrouka.games>
2022-01-01 17:51:19 +0300

    Revert "Fixed bug with double BoardManager, cleaned up some files"
    This reverts commit 1da3f6fb61489cfeb5a1c4a3778ea913e66dce08.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
     public int playerFoodPoints = 100;
 
     [HideInInspector] public bool isPlayerTurn = true;
-    private int level;
+    private int level = 1;
     private List<Enemy> enemies;
     private bool isEnemiesMoving;
 
@@ -45,6 +45,7 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
         enemies = new List<Enemy>();
         boardManager = GetComponent<BoardManager>();
+        InitGame();
     }
 
     private void OnLevelFinishLoading(Scene scene, LoadSceneMode mode)
